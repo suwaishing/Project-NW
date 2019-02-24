e0665a67d3533192d5d4e62d4f1a28e55f377c51
winston96 <w.shing96@gmail.com>
2019-02-24 19:15:01 +0700

    fix linear style

--- src/app/net-worth/net-worth.component.ts
+++ src/app/net-worth/net-worth.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'net-worth',
@@ -6,10 +7,77 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./net-worth.component.css']
 })
 export class NetWorthComponent implements OnInit {
-
+  netWorth: FormGroup;
+  cashOnHand: FormControl;
+  cashInBank: FormControl;
+  
+  house: FormControl;
+  otherRealEstate: FormControl;
+  stock: FormControl;
+  bond: FormControl;
+  otherInvestment: FormControl;
+  vehicle: FormControl;
+  jewelry: FormControl;
+  personalProperty: FormControl;
+  mortgage: FormControl;
+  loan: FormControl;
+  creditCard: FormControl;
+  studentLoans: FormControl;
+  otherDebt: FormControl;
   constructor() { }
 
-  ngOnInit() {
+  createFormControls() {
+    this.cashOnHand = new FormControl('');
+    this.cashInBank = new FormControl('');
+    this.house = new FormControl('');
+    this.otherRealEstate = new FormControl('');
+    this.stock = new FormControl('');
+    this.bond = new FormControl('');
+    this.otherInvestment = new FormControl('');
+    this.vehicle = new FormControl('');
+    this.jewelry = new FormControl('');
+    this.personalProperty = new FormControl('');
+    this.mortgage = new FormControl('');
+    this.loan = new FormControl('');
+    this.creditCard = new FormControl('');
+    this.studentLoans = new FormControl('');
+    this.otherDebt = new FormControl('');
   }
 
+  createForm() {
+    this.netWorth = new FormGroup({
+      cashAndEquivalent: new FormGroup({
+        cashOnHand: this.cashOnHand,
+        cashInBank: this.cashInBank
+      }),
+      realEstate: new FormGroup({
+        house: this.house,
+        otherRealEstate: this.otherRealEstate
+      }),
+      investment: new FormGroup({
+        stock: this.stock,
+        bond: this.bond,
+        otherInvestment: this.otherInvestment
+      }),
+      personalAssets: new FormGroup({
+        vehicle: this.vehicle,
+        jewelry: this.jewelry,
+        personalProperty: this.personalProperty
+      }),
+      Liability: new FormGroup({
+        mortgage: this.mortgage,
+        loan: this.loan,
+        creditCard: this.creditCard,
+        studentLoans: this.studentLoans,
+        otherDebt: this.otherDebt
+      })
+    });
+  }
+  ngOnInit() {
+    this.createFormControls();
+    this.createForm();
+  }
+  onSubmit() {
+    console.log(this.netWorth.value)
+  }
 }
