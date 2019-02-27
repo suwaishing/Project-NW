8f4eb1bb4f304d0f8b0af3d59859985b496d7634
rozetta1125 <rozetta1125@gmail.com>
2019-02-27 16:54:48 +0700

    delete stuffs

--- src/app/shared/remove-comma.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { RemoveCommaPipe } from './remove-comma.pipe';
-
-describe('RemoveCommaPipe', () => {
-  it('create an instance', () => {
-    const pipe = new RemoveCommaPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- src/app/shared/space3-digits.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { Space3DigitsDirective } from './space3-digits.directive';
-
-describe('Space3DigitsDirective', () => {
-  it('should create an instance', () => {
-    const directive = new Space3DigitsDirective();
-    expect(directive).toBeTruthy();
-  });
-});
