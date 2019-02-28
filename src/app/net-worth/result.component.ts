f0ea31ddb53a37e6f4e67105f20535c52ca77a7b
winston96 <w.shing96@gmail.com>
2019-02-28 12:57:19 +0700

    add currency pipe, complete result form

--- src/app/net-worth/result.component.ts
+++ src/app/net-worth/result.component.ts
@@ -3,7 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'result',
   templateUrl: './result.component.html',
-  styles: ['']
+  styles: [`
+    .layout-width{
+      width: 100%;
+      max-width: 786px;
+      margin: 0 auto;
+    }
+  `]
 })
 export class ResultComponent {
 
