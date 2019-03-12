3fd172a7ea0e6f9d3e1376f5a07bff0778c8cf7b
rozetta1125 <rozetta1125@gmail.com>
2019-03-12 19:54:51 +0700

    design v2

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,14 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+declare var $:any;
+
+
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  styleUrls: ['./nav.component.scss']
 })
 export class NavComponent implements OnInit {
-
-  constructor() { }
-
+  constructor(@Inject(DOCUMENT) private document: Document) { }
+  menuOpened:boolean=false;
+  menuClosed:boolean=true;
+  menuClicked:boolean=false;
+  openMenu(){
+    this.menuClicked=true;
+    this.menuOpened=!this.menuOpened;
+    this.menuClosed=!this.menuClosed;
+    this.document.body.classList.toggle('disabled-scroll');
+  }
   ngOnInit() {
   }
 
