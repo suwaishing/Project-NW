4884b1b06dcf2e7377bcac69492225e16e8cabd7
rozetta1125 <rozetta1125@gmail.com>
2019-03-31 13:48:56 +0700

    loader v0.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+declare var $:any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'Project';
+export class AppComponent{
+
 }
