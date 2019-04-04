2ec736ff825792c4e44c10d97377ded47748d934
winston96 <w.shing96@gmail.com>
2019-04-04 19:21:11 +0700

    404 v0.2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Angulartics2GoogleAnalytics } from 'angulartics2/ga';
 declare var $:any;
 
@@ -7,14 +7,21 @@ declare var $:any;
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent{
+export class AppComponent implements OnInit{
   
   constructor(angulartics2GoogleAnalytics: Angulartics2GoogleAnalytics){
-    this.svgHide();
     angulartics2GoogleAnalytics.startTracking();
   }
-
+  isSvgHide=false;
   svgHide(){
-    setTimeout(()=>$('.svg-div').addClass('svg-hidden'),2500)
+    setTimeout(()=>
+      $('.svg-div').addClass('svg-hidden')
+    ,2500);
+  }
+  ngOnInit(){
+    setTimeout(()=>
+      this.isSvgHide=true
+    ,2600);
+    this.svgHide();
   }
 }
